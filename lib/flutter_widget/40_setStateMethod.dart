e25f193604ffe200e8818246d78123a94d616ed3
mansukh makwana <mansukh.m@celcius.in>
2025-06-11 14:56:08 +0530

    setState Method

---
+++ lib/flutter_widget/40_setStateMethod.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DemoApp());
+}
+
+class DemoApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return DemoApppState();
+  }
+}
+
+class DemoApppState extends State<DemoApp> {
+  var num = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              num++;
+            });
+          },
+          child: Text("$num"),
+        ),
+      ),
+    );
+  }
+}
