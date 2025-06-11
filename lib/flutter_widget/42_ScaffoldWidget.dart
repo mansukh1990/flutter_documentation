04f0d614d684267c2735ac4afe34f96c1b1e2f18
mansukh makwana <mansukh.m@celcius.in>
2025-06-11 15:29:34 +0530

    Scaffold Widget

---
+++ lib/flutter_widget/42_ScaffoldWidget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DemoApp());
+}
+
+class DemoApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => DemoAppState();
+}
+
+class DemoAppState extends State<DemoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(title: Text("App Title"), backgroundColor: Colors.blue),
+        body: Center(child: Text("Hello")),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_chart),
+              label: "Chart",
+            ),
+          ],
+        ),
+        drawer: Drawer(child: Text("Demo")),
+      ),
+    );
+  }
+}
