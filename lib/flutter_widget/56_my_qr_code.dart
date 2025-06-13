dda20f1331fcd1b5351758c917ab10624af78d89
mansukh makwana <mansukh.m@celcius.in>
2025-06-13 10:55:44 +0530

    SafeArea Widget

--- lib/flutter_widget/56_my_qr_code.dart
+++ lib/flutter_widget/56_my_qr_code.dart
@@ -8,8 +8,6 @@ class QRCode extends StatelessWidget {
   var url =
       'https://pixabay.com/get/gaf372b7be03e9f1855448861546e9c3bdb9ebd1dff5f893889b1ad4756b0c4e15a97bc7acab7cf46e9a951e5029b1f4e1b4583d457f84626dea17887e0f80a74_1280.jpg';
 
-  QRCode({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
---
+++ lib/flutter_widget/57_safearea_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SafeAreaApp());
+}
+
+class SafeAreaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          minimum: EdgeInsets.all(20),
+          top: true,
+          child: Text(
+            "Hello",
+            style: TextStyle(fontSize: 50, color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+}
