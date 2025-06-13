3f6689e6d35934824dc0fd30d3ffba0c51c305bd
mansukh makwana <mansukh.m@celcius.in>
2025-06-13 11:17:25 +0530

    Google Font

---
+++ lib/flutter_widget/58_google_font.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+main() {
+  runApp(GoogleFontApp());
+}
+
+class GoogleFontApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "GoogleFontApp",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Google Font"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Text(
+            "Hello",
+            style: GoogleFonts.openSans(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
