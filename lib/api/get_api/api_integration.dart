93fc9aa507686031dccb52072816f133e905bd96
mansukh makwana <mansukh.m@celcius.in>
2025-06-18 18:14:13 +0530

    GET api Example

---
+++ lib/api/get_api/api_integration.dart
@@ -0,0 +1,26 @@
+import 'package:dart/api/get_api/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: Colors.blue),
+        body: HomePage(),
+      ),
+    );
+  }
+}
